4c0cca6772b91df3a312a664e624f3ce5a75b964
Cal Morrison <cmorrison2@upei.ca>
2021-03-10 00:59:24 -0400

    Added Tailwind and Basic Page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#161e2e',
+        'light-blue': '#22d3ee',
+      },
+    },
+  },
+  variants: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
